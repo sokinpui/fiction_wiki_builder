c3bcaf07c47c2b9318f8a0bd7b5ee00617abebb6
sokinpui <sosokinpui@gmail.com>
2025-05-30 11:38:42 +0800

    update script and .gitignore

---
+++ fivebooks.sh
@@ -0,0 +1,9 @@
+python scripts/crawl_fiction.py --threads 20 --output books --start_chapter 1 --end_chapter 4000 "https://8520b295ef767.fk6k.cc/index/46120/1.html"
+
+python scripts/crawl_fiction.py --threads 20 --output books --start_chapter 1 --end_chapter 4000 "https://8520b295ef767.fk6k.cc/index/45908/1.html"
+
+python scripts/crawl_fiction.py --threads 20 --output books --start_chapter 1 --end_chapter 4000 "https://8520b295ef767.fk6k.cc/index/45867/1.html"
+
+python scripts/crawl_fiction.py --threads 20 --output books --start_chapter 1 --end_chapter 4000 "https://8520b295ef767.fk6k.cc/index/45862/1.html"
+
+python scripts/crawl_fiction.py --threads 20 --output books --start_chapter 1 --end_chapter 4000 "https://8520b295ef767.fk6k.cc/index/41814/1.html"
